4f49ce003edc25f807fdfc0bb53f7b90f10cad93
Jordan Tobin <46364302+GrimesGolden@users.noreply.github.com>
2023-08-09 11:36:06 -0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+using std::cout;
+
+namespace X {
+	int var;
+	void print() {
+		cout << var;
+	}
+}
+
+namespace Y {
+	int var;
+	void print() {
+		cout << var;
+	}
+}
+
+namespace Z {
+	int var;
+	void print() {
+		cout << var;
+	}
+}
